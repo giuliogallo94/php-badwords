2b615ae106654dbcbf90e498d84cdead85341f46
giuliogallo94 <giuliogallo1994@gmail.com>
2023-12-11 17:00:40 +0100

    add: work

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="words.php" method="GET">
+        <label for="paragraph">Paragrafo:</label>
+        <input type="text" id="paragraph" name="paragraph" >
+
+        <label for="badword">Parola da censurare:</label>
+        <input type="text" id="badword" name="badword">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
---
+++ words.php
@@ -0,0 +1,41 @@
+<?php
+$paragraph = $_GET['paragraph'];
+$badword = $_GET['badword'];
+$p_length = strlen($paragraph);
+$censor = str_replace($badword, '***', $paragraph);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="paragraph">
+        <h1>Il tuo paragrafo:</h1>
+        <p><?php echo "$paragraph" ?></p>
+        <p>Il paragrafo da te inserito è lungo:
+            <?php
+            echo $p_length;
+            ?>
+            caratteri
+        </p>
+    </div>
+
+    <div class="censored">
+        <h1>Il tuo paragrafo censurato:</h1>
+        <p><?php echo "$censor" ?></p>
+        <p>Il paragrafo da te inserito è lungo:
+            <?php
+            echo $p_length;
+            ?>
+            caratteri
+        </p>
+    </div>
+</body>
+
+</html>
